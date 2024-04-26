23301b882ee18b64dfbc5af61b1274e5c67f1fb8
Julian Bilcke <julian.bilcke@huggingface.co>
2024-04-26 18:45:43 +0200

    release 0.0.3

--- packages/aitube-clap/src/index.ts
+++ packages/aitube-clap/src/index.ts
@@ -21,6 +21,5 @@ export {
 export { newClap, newModel, newSegment } from './factories'
 export { parseClap, serializeClap, fetchClap } from './io'
 export { generateSeed, getClapAssetSourceType, getValidNumber, isValidNumber } from './utils'
-
-// let's not add those helpers by default, there add some weight while not being essentials
-// export { generateClapFromSimpleStory, getEmptyClap } from './helpers'
+export { blobToDataUri, dataUriToBlob, clapToDataUri } from './converters'
+export { generateClapFromSimpleStory, getEmptyClap } from './helpers'
