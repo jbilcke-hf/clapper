cb147e4528707c9757de0ccd1c0fddebae48e83d
Julian Bilcke <julian.bilcke@huggingface.co>
2024-05-05 15:17:41 +0200

    bump to 0.0.14

---
+++ packages/aitube-client/src/utils/applyClapCompletion.ts
@@ -0,0 +1,21 @@
+import { ClapProject, updateClap } from "@aitube/clap"
+
+import { ClapCompletionMode } from ".."
+
+export async function applyClapCompletion(
+  existingClap: ClapProject,
+  newerClap: ClapProject,
+  clapCompletionMode: ClapCompletionMode
+): Promise<ClapProject> {
+  // in both those mode we leave full control to what is inside "newerClap"
+  if (clapCompletionMode === ClapCompletionMode.FULL || clapCompletionMode === ClapCompletionMode.PARTIAL) {
+    return newerClap
+  }
+
+  // else we are in ClapCompletionMode.MERGE or ClapCompletionMode.REPLACE
+  const result = await updateClap(existingClap, newerClap, {
+    inlineReplace: clapCompletionMode === ClapCompletionMode.REPLACE
+  })
+
+  return result
+}
