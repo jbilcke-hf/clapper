27f60c29e9486cedbb71bd6214fb88a84050ed22
Julian Bilcke <julian.bilcke@huggingface.co>
2024-05-27 17:02:21 +0200

    0.0.8

--- packages/broadway/src/utils/index.ts
+++ packages/broadway/src/utils/index.ts
@@ -1,13 +1,7 @@
 export { cleanUTF8Characters } from "./cleanUTF8Characters"
 export { deduplicate } from "./deduplicate"
 export { getEntities } from "./getEntities"
-
-  // unfortunately we cannot use this kind of export right now,
-  // as this is Node-only, and breaks when used in the browser
-// export { getRandomDirectory } from "./getRandomDirectory"
 export { isAllCaps } from "./isAllCaps"
 export { onlyContainsStrangeNumber } from "./onlyContainsStrangeNumber"
 export { pick } from "./pick"
-export { readLocalOrRemotePlainText } from "./readLocalOrRemotePlainText"
-export { readPlainText } from "./readPlainText"
 export { sleep } from "./sleep"
