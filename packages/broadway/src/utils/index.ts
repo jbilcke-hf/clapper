1f34a58d9d5f6c87f10fa1f0b9a1fe3101b54963
Julian Bilcke <julian.bilcke@huggingface.co>
2024-05-27 16:34:46 +0200

    0.0.7 - disable Node imports

--- packages/broadway/src/utils/index.ts
+++ packages/broadway/src/utils/index.ts
@@ -1,6 +1,9 @@
 export { cleanUTF8Characters } from "./cleanUTF8Characters"
 export { deduplicate } from "./deduplicate"
 export { getEntities } from "./getEntities"
+
+  // unfortunately we cannot use this kind of export right now,
+  // as this is Node-only, and breaks when used in the browser
 // export { getRandomDirectory } from "./getRandomDirectory"
 export { isAllCaps } from "./isAllCaps"
 export { onlyContainsStrangeNumber } from "./onlyContainsStrangeNumber"
