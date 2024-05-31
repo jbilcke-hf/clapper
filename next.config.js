d7221183602afdfa8deed6695d19cf3b7084f000
Julian Bilcke <julian.bilcke@huggingface.co>
2024-05-31 13:31:30 +0200

    use next start

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-
   experimental: {
     serverActions: {
       // a clap file can be quite large - but that's OK
