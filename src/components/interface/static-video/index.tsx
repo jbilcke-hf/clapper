f153239e55d7c942627321a7b60fd329e7476d7a
Julian Bilcke <julian.bilcke@huggingface.co>
2024-06-03 01:01:01 +0200

    update timeline

---
+++ src/components/interface/static-video/index.tsx
@@ -0,0 +1,84 @@
+import React, { ReactNode, useEffect, useRef } from "react"
+
+import { useTimelineState } from "@aitube/timeline"
+import useRequestAnimationFrame from "@/lib/hooks/useRequestAnimationFrame"
+
+export function StaticVideo({
+  video = "",
+  isBusy = false,
+  progress = 0,
+  status = "",
+  error = "",
+  children = undefined,
+}: {
+  video: string
+  isBusy: boolean
+  progress: number
+  status: string
+  error: ReactNode
+  children?: ReactNode
+} = {
+  video: "",
+  isBusy: false,
+  progress: 0,
+  status: "",
+  error: "",
+  children: undefined,
+}) {
+
+  const setCursorTimestampAt = useTimelineState(s => s.setCursorTimestampAt)
+
+  const ref = useRef<HTMLVideoElement>(null)
+  
+  
+  useRequestAnimationFrame(() => {
+    if (!ref.current) { return }
+    setCursorTimestampAt(ref.current.currentTime)
+  })
+
+  const placeholder = <div
+    className="
+      text-base
+      text-center
+      text-stone-50/90 dark:text-stone-50/90
+      "
+    >{
+      error ? <span>{error}</span> : 
+      <span>No video yet</span>
+    }</div>
+
+  const hasVideoContent = Boolean(video && video?.length > 128)
+
+  return (
+    <>{
+      children ? children : isBusy ? <div className="
+    flex flex-col 
+    items-center justify-center
+    w-full h-full
+    text-center space-y-1.5">
+      <p className="text-2xl font-bold">{progress}%</p> 
+      <p className="text-base text-white/70">{
+      status
+        ? status
+        : error
+        ? <span>{error}</span>
+        : placeholder // to prevent layout changes
+      }</p>
+      </div>
+    : hasVideoContent ? <video
+      ref={ref}
+      src={video}
+      controls
+      playsInline
+      // I think we can't autoplay with sound,
+      // so let's disable auto-play
+      // autoPlay
+      // muted
+      loop
+      className="h-full object-cover"
+      style={{
+      }}
+    /> : placeholder
+  }</>
+  )
+}
---
+++ src/lib/hooks/useRequestAnimationFrame.ts
@@ -0,0 +1,61 @@
+import * as React from 'react'
+
+type Config = {
+  duration?: number
+  shouldAnimate?: boolean
+}
+
+/**
+ * @param nextAnimationFrameHandler Function to be called before the browser performs the next repaint. The number of callbacks is usually 60 times per second, but will generally match the display refresh rate in most web browsers.
+ * @param duration Animation duration. If present, affects `progress` value passed to `nextAnimationFrameHandler` (0 < `progress` <=1). Default value: POSITIVE_INFINITY - for infinite animation.
+ * @param shouldAnimate Turn animation on/off. Default value: true - enable animation from the first render
+ *
+ * @description keep your `nextAnimationFrameHandler` as simple and performant as possible with the least amount of dependencies and transformations. It will be called frequently and this can lead to bad UX
+ */
+const useRequestAnimationFrame = (
+  nextAnimationFrameHandler: (progress: number) => void,
+  {
+    duration = Number.POSITIVE_INFINITY,
+    shouldAnimate = true
+  }: Config = {
+    duration: Number.POSITIVE_INFINITY,
+    shouldAnimate: true }
+) => {
+  const isServerSideOrApiUnsupported =
+    typeof requestAnimationFrame === 'undefined' ||
+    typeof window === 'undefined'
+  if (isServerSideOrApiUnsupported) {
+    return
+  }
+
+  const frame = React.useRef(0)
+  const firstFrameTime = React.useRef(performance.now())
+
+  const animate = (now: number) => {
+    // calculate at what time fraction we are currently of whole time of animation
+    let timeFraction = (now - firstFrameTime.current) / duration
+    if (timeFraction > 1) {
+      timeFraction = 1
+    }
+
+    if (timeFraction <= 1) {
+      nextAnimationFrameHandler(timeFraction)
+
+      // request next frame only in cases when we not reached 100% of duration
+      if (timeFraction != 1) frame.current = requestAnimationFrame(animate)
+    }
+  }
+
+  React.useEffect(() => {
+    if (shouldAnimate) {
+      firstFrameTime.current = performance.now()
+      frame.current = requestAnimationFrame(animate)
+    } else {
+      cancelAnimationFrame(frame.current)
+    }
+
+    return () => cancelAnimationFrame(frame.current)
+  }, [shouldAnimate])
+}
+
+export default useRequestAnimationFrame
