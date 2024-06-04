5aadd46eaca138321b2235ca18ad4297aec1941d
Julian Bilcke <julian.bilcke@huggingface.co>
2024-06-04 15:03:39 +0200

    use the official domain

--- src/lib/core/constants.ts
+++ src/lib/core/constants.ts
@@ -6,9 +6,5 @@ export const HARD_LIMIT_NB_MAX_ASSETS_TO_GENERATE_IN_PARALLEL = 32
 export const APP_NAME = "Clapper"
 export const APP_REVISION = "nightly 20240604"
 
-// TODO: use clapper.app
-// export const APP_DOMAIN = "Clapper.app"
-// export const APP_LINK = "https://clapper.app"
-
-export const APP_DOMAIN = "jbilcke-hf-clapper.hf.space"
-export const APP_LINK = "https://jbilcke-hf-clapper.hf.space"
+export const APP_DOMAIN = "Clapper.app"
+export const APP_LINK = "https://clapper.app"
