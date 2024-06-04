aa8a33b5c008f380b18355280206247f1b4352ce
Julian Bilcke <julian.bilcke@huggingface.co>
2024-06-04 15:10:29 +0200

    update disclaimer

--- src/components/interface/iframe-warning/index.tsx
+++ src/components/interface/iframe-warning/index.tsx
@@ -8,7 +8,7 @@ export function IframeWarning() {
   const [showWarning, setShowWarning] = useState(false)
 
   useEffect(() => {
-    setShowWarning(window.self !== window.top)
+    setShowWarning(true) // window.self !== window.top)
     return () => {}
   }, [])
   // TODO: read our global state
@@ -16,13 +16,15 @@ export function IframeWarning() {
   return (
     <div className={cn(`
       fixed
-      z-50
+      z-[60]
       flex
       w-screen h-screen
       top-0 left-0 right-0 bottom-0
       p-0 m-0
       overflow-hidden
       items-center justify-center
+      text-center
+      bg-stone-950
       `,
       showWarning
       ? 'opacity-100 pointer-events-auto'
@@ -32,13 +34,14 @@ export function IframeWarning() {
        backgroundImage: "repeating-radial-gradient( circle at 0 0, transparent 0, #000000 7px ), repeating-linear-gradient( #34353655, #343536 )"
       }}>
       <p
-        className={cn("text-stone-100 font-sans font-thin text-[3vw]")}
+        className={cn("text-stone-100 font-sans")}
         style={{ textShadow: "#000 1px 0 3px" }}>
-        <span>
-          {APP_NAME} doesn&apos;t work well when used from an iframe, please use the official website instead:
+        <span className="font-thin text-[2vw]">
+          {APP_NAME} doesn&apos;t support iframe embedding yet,
+          <br/>please use the official domain instead:
         </span>
         <br/>
-        <a href={APP_LINK} target="_blank">
+        <a href={APP_LINK} className="font-regular text-[4vw] underline underline-offset-[1vw]" target="_blank">
           {APP_DOMAIN}
         </a>
       </p>
