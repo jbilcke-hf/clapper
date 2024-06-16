46d3de12bdab49368ffab4d68347a53cf69cda5f
Julian Bilcke <julian.bilcke@huggingface.co>
2024-06-17 01:13:12 +0200

    0.0.22

--- packages/aitube-engine/src/prompts/getCharacterPrompt.ts
+++ packages/aitube-engine/src/prompts/getCharacterPrompt.ts
@@ -1,26 +1,14 @@
 import { ClapEntity } from "@aitube/clap"
 
 export function getCharacterPrompt(entity: ClapEntity): string {
+  const characterPrompt = [
+    entity.age ? `${entity.age}yo` : '', // 34yo
+    entity.region ? `${entity.region}` : '', // american
+    entity.gender !== "object" ? entity.gender : 'person', // woman
+    entity.label ? `named ${entity.label}` : '', // Jessica
+    entity.appearance ? `${entity.appearance}` : 'speaking', // blond hair
+    entity.description ? `${entity.description}` : '', // blond hair
+  ].map(i => i.trim()).filter(i => i).join(" ").trim()
 
-  let characterPrompt = ""
-  if (entity.description) {
-    characterPrompt = [
-      // the label (character name) can help making the prompt more unique
-      // this might backfires however, if the name is
-      // something like "SUN", "SILVER" etc
-      // I'm not sure stable diffusion really needs this,
-      // so let's skip it for now (might still be useful for locations, though)
-      // we also want to avoid triggering "famous people" (BARBOSSA etc)
-      // entity.label,
-
-      entity.description
-    ].join(", ")
-  } else {
-    characterPrompt = [
-      entity.gender !== "object" ? entity.gender : "",
-      entity.age ? `aged ${entity.age}yo` : '',
-      entity.label ? `named ${entity.label}` : '',
-    ].map(i => i.trim()).filter(i => i).join(", ")
-  }
   return characterPrompt
 }
