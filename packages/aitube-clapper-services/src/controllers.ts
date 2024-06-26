2582d4ddfa2c2dd6482b117f7a84f1a5df619867
Julian Bilcke <julian.bilcke@huggingface.co>
2024-06-26 12:55:45 +0200

    0.0.2

--- packages/aitube-clapper-services/src/controllers.ts
+++ packages/aitube-clapper-services/src/controllers.ts
@@ -7,12 +7,14 @@ import { MonitorStore } from "./monitor"
 import { RendererStore } from "./renderer"
 import { PluginsStore } from "./plugins"
 import { AudioStore } from "./audio"
+import { TasksStore } from "./tasks"
 
 export type AudioController = UseBoundStore<StoreApi<AudioStore>>
 export type BroadcastController = UseBoundStore<StoreApi<BroadcastStore>>
 export type EditorController = UseBoundStore<StoreApi<EditorStore>>
 export type MonitorController = UseBoundStore<StoreApi<MonitorStore>>
 export type RendererController = UseBoundStore<StoreApi<RendererStore>>
+export type TasksController = UseBoundStore<StoreApi<TasksStore>>
 export type TimelineController = UseBoundStore<StoreApi<TimelineStore>>
 export type PluginsController = UseBoundStore<StoreApi<PluginsStore>>
 
@@ -24,6 +26,7 @@ export type PrivateControllers = {
 // missing from this list are the settings and the plugins store itself
 export type PublicControllers = {
   audio: AudioController
+  tasks: TasksController
   editor: EditorController
   monitor: MonitorController
   timeline: TimelineController
