29b874021be9f69483d272480be698e23125f6c8
Julian Bilcke <julian.bilcke@huggingface.co>
2024-07-12 14:16:37 +0200

    0.0.21

--- packages/aitube-clapper-services/src/entity-editor.ts
+++ packages/aitube-clapper-services/src/entity-editor.ts
@@ -3,15 +3,16 @@ import { ClapEntity } from "@aitube/clap"
 import { VersionControls, VersionedState } from "./version-control"
 
 export type EntityEditorState = {
-
+  draft?: ClapEntity
+  showEntityList: boolean
 } & VersionedState<ClapEntity>
 
 export type EntityEditorControls = {
+  setDraft: (draft?: ClapEntity) => void
   selectEntity: (id: string) => void
   addEntity: (entity: ClapEntity) => void
   removeEntity: (id: string) => void
-  updateEntity: (updatedEntity: ClapEntity) => void
-  loadEntities: (entities: ClapEntity[]) => void
+  setShowEntityList: (showEntityList: boolean) => void
 } & VersionControls<ClapEntity>
 
 export type EntityEditorStore = EntityEditorState & EntityEditorControls
