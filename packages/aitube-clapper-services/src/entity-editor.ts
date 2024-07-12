62e239ea138f14720006f1cadf9a7952cc89461a
Julian Bilcke <julian.bilcke@huggingface.co>
2024-07-12 13:48:57 +0200

    0.0.20

--- packages/aitube-clapper-services/src/entity-editor.ts
+++ packages/aitube-clapper-services/src/entity-editor.ts
@@ -7,7 +7,11 @@ export type EntityEditorState = {
 } & VersionedState<ClapEntity>
 
 export type EntityEditorControls = {
-
+  selectEntity: (id: string) => void
+  addEntity: (entity: ClapEntity) => void
+  removeEntity: (id: string) => void
+  updateEntity: (updatedEntity: ClapEntity) => void
+  loadEntities: (entities: ClapEntity[]) => void
 } & VersionControls<ClapEntity>
 
 export type EntityEditorStore = EntityEditorState & EntityEditorControls
