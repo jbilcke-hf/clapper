646e91adb24453626247e2181740a53b65ac90cd
Daniel Mauricio Flores <dnielfs@gmail.com>
2024-07-13 16:39:56 +0200

    fix: use binary encoding before creating buffer in base64DataUriToBlob

--- src/lib/utils/base64DataUriToBlob.ts
+++ src/lib/utils/base64DataUriToBlob.ts
@@ -3,7 +3,7 @@ export function base64DataUriToBlob(dataURI: string) {
 
   const match = dataURI.match(/(?:image|video|audio|text)\/[^;]+/)
   const type = match?.[0] || ""
-  const base64 = dataURI.replace(/^[^,]+,/, '');
+  const base64 = Buffer.from(dataURI.replace(/^[^,]+,/, ''), 'base64').toString('binary');
   const arrayBuffer = new ArrayBuffer(base64.length);
   const typedArray = new Uint8Array(arrayBuffer);
 
