d176eeeb6d3cefab98882a9a9bdac81791245a81
Julian Bilcke <julian.bilcke@huggingface.co>
2024-07-18 12:40:33 +0200

    trying workarounds..

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp']
+    formats: ['image/avif', 'image/webp', 'image/png']
   },
   experimental: {
     serverActions: {
