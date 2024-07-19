f0dafdee06d29672547957e0fd6be9bb2e30321f
Julian Bilcke <julian.bilcke@huggingface.co>
2024-07-19 16:51:53 +0200

    oh

--- src/services/ui/getDefaultUIState.ts
+++ src/services/ui/getDefaultUIState.ts
@@ -1,4 +1,4 @@
-import { SettingsCategory, UIState } from '@aitube/clapper-services'
+import { ProjectCreationWizardStep, SettingsCategory, UIState } from '@aitube/clapper-services'
 
 export function getDefaultUIState(): UIState {
   const state: UIState = {
@@ -14,6 +14,7 @@ export function getDefaultUIState(): UIState {
     showFPS: false,
     followCursor: false,
     editorFontSize: 12,
+    projectCreationWizardStep: ProjectCreationWizardStep.NONE,
   }
 
   return state
