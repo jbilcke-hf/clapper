856695de6a05aea591a0bf8319d7774eb04f2bce
Julian Bilcke <julian.bilcke@huggingface.co>
2024-07-24 15:26:34 +0200

    0.1.1

--- packages/aitube-timeline/src/utils/getFinalVideo.ts
+++ packages/aitube-timeline/src/utils/getFinalVideo.ts
@@ -1,4 +1,4 @@
-import { ClapProject, ClapSegment, ClapSegmentCategory } from "@aitube/clap"
+import { ClapProject, ClapSegment, ClapSegmentCategory, ClapSegmentStatus } from "@aitube/clap"
 
 import { TimelineSegment } from "@/types"
 import { clapSegmentToTimelineSegment } from "./clapSegmentToTimelineSegment"
@@ -6,7 +6,7 @@ import { clapSegmentToTimelineSegment } from "./clapSegmentToTimelineSegment"
 export async function getFinalVideo(clap: ClapProject): Promise<TimelineSegment | undefined> {
   const alreadyAnEmbeddedFinalVideo: ClapSegment | undefined = clap.segments.filter(s =>
     s.category === ClapSegmentCategory.VIDEO &&
-    s.status === "completed" &&
+    s.status === ClapSegmentStatus.COMPLETED &&
     s.startTimeInMs === 0 &&
     s.endTimeInMs === clap.meta.durationInMs &&
     s.assetUrl).at(0)
