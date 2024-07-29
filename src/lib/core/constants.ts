0461b682612d797338fd7940ce8bdc62689359a5
Julian Bilcke <julian.bilcke@huggingface.co>
2024-07-29 11:07:07 +0200

    bump

--- src/lib/core/constants.ts
+++ src/lib/core/constants.ts
@@ -3,7 +3,7 @@
 export const HARD_LIMIT_NB_MAX_ASSETS_TO_GENERATE_IN_PARALLEL = 32
 
 export const APP_NAME = 'Clapper.app'
-export const APP_REVISION = '20240727+2321'
+export const APP_REVISION = '20240729+1107'
 
 export const APP_DOMAIN = 'Clapper.app'
 export const APP_LINK = 'https://clapper.app'
