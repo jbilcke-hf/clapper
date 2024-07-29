86d227b8630fe9bdfbb4054252c8f573564aba81
Julian Bilcke <julian.bilcke@huggingface.co>
2024-07-29 21:54:58 +0200

    0.1.2-12

--- packages/aitube-clapper-services/src/filter-editor.ts
+++ packages/aitube-clapper-services/src/filter-editor.ts
@@ -9,16 +9,26 @@ export type Filter = {
   shader: string // WGSL shader code for the filter
 }
 
+export type FilterParams = Record<string, string | number | boolean> 
+
+export type FilterWithParams = {
+  filter: Filter
+  parameters: FilterParams
+}
+
 export type FilterEditorState = {
+  isEnabled: boolean
   availableFilters: Filter[]
-  activeFilters: Filter[]
+  activeFilters: FilterWithParams[]
 }& VersionState<string>
 
 export type FilterEditorControls = {
-  applyFilters: (input: string) => Promise<string>
+  runFilterPipeline: (input: string) => Promise<string>
 
+  setEnabled: (isEnabled: boolean) => void
+  
   // later we can have functions like addFilter()
   // which could be used by the plugin engine to add filters
-} & VersionControls<string>
+} & VersionControls<FilterWithParams[]>
 
 export type FilterEditorStore = FilterEditorState & FilterEditorControls
