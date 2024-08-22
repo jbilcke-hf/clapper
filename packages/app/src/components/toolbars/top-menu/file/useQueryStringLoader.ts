64ab55d8cdf469547c88fc6021deda9151e2e856
Julian Bilcke <julian.bilcke@huggingface.co>
2024-08-22 20:42:33 +0200

    adding more url params

--- packages/app/src/components/toolbars/top-menu/file/useQueryStringLoader.ts
+++ packages/app/src/components/toolbars/top-menu/file/useQueryStringLoader.ts
@@ -3,24 +3,31 @@
 import { useEffect } from 'react'
 
 import { useQueryStringParams } from '@/lib/hooks'
-import { useIO } from '@/services'
+import { useAssistant, useIO } from '@/services'
 
 export function useQueryStringLoader() {
-  const { clapUrl } = useQueryStringParams({
+  const { clapUrl, startAt, interactive, prompt, imageStrategy } = useQueryStringParams({
     // clapUrl: `/samples/test.clap`,
     // clapUrl: `/samples/Afterglow%20v10%20X%20Rewrite%20Bryan%20E.%20Harris%202023.clap`,
-    clapUrl: '',
   })
+
+  const processUserMessage = useAssistant(s => s.processUserMessage)
   const openClapUrl = useIO((s) => s.openClapUrl)
 
   useEffect(() => {
     ;(async () => {
-      if (!clapUrl) {
+      if (prompt.length > 2) {
+        const message = `please create the first scene's segments of a new story, based on this simple pitch (you can expand it): ${prompt}`
+
+        console.log('initializing using message', prompt)
+        processUserMessage(message)
+      } else if (clapUrl) {
+        console.log(`loading ${clapUrl}`)
+        await openClapUrl(clapUrl)
+      } else {
         console.log('No clap URL provided')
         return
       }
-      await openClapUrl(clapUrl)
     })()
-  }, [openClapUrl, clapUrl])
-
+  }, [prompt, openClapUrl, clapUrl, interactive, imageStrategy])
 }
