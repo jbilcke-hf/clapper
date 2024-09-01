6b56adc0d44d58e3f170a82f5ad373bec76391db
Julian Bilcke <julian.bilcke@huggingface.co>
2024-09-02 01:29:24 +0200

    fix issues

---
+++ packages/client/src/constants/config.ts
@@ -0,0 +1,16 @@
+import { defaultAitubeHostname } from "./defaultValues"
+
+// we leave the opportunity to override this at runtime
+export const aitubeUrl = `${
+  process.env.AITUBE_URL ||
+  `https://${defaultAitubeHostname}`
+}`
+
+// note: let's keep it simple and only support one version at a time
+export const aitubeApiVersion = "v1"
+
+export const aitubeApiUrl = `${
+  aitubeUrl
+}${
+  aitubeUrl.endsWith("/") ? "" : "/"
+}api/${aitubeApiVersion}/`
---
+++ packages/client/src/utils/applyClapCompletion.ts
@@ -0,0 +1,21 @@
+import { ClapCompletionMode, ClapProject, updateClap } from "@aitube/clap"
+
+export async function applyClapCompletion(
+  existingClap: ClapProject,
+  newerClap: ClapProject,
+  clapCompletionMode: ClapCompletionMode
+): Promise<ClapProject> {
+  // in both those mode we leave full control to what is inside "newerClap"
+  if (clapCompletionMode === ClapCompletionMode.FULL || clapCompletionMode === ClapCompletionMode.PARTIAL) {
+    return newerClap
+  }
+
+  // else we are in ClapCompletionMode.MERGE or ClapCompletionMode.REPLACE
+  const result = await updateClap(existingClap, newerClap, {
+    // the newer clap meta may contain incomplete information
+    overwriteMeta: false,
+    inlineReplace: clapCompletionMode === ClapCompletionMode.REPLACE
+  })
+
+  return result
+}
