bc8b1c1bb27244380a50a5b60b063754e8388985
Julian Bilcke <julian.bilcke@huggingface.co>
2024-09-04 01:12:18 +0200

    add vu meter

--- packages/app/src/components/monitor/index.tsx
+++ packages/app/src/components/monitor/index.tsx
@@ -8,6 +8,7 @@ import { PlayerControls } from './PlayerControls'
 import { useUI } from '@/services/ui'
 import { useTheme } from '@/services/ui/useTheme'
 import { UIWindowLayout } from '@aitube/clapper-services'
+import { VUMeter } from './VUMeter'
 
 export function Monitor() {
   const [isFullscreen, setFullscreen, ref] = useFullscreenStatus()
@@ -18,30 +19,37 @@ export function Monitor() {
     <div
       ref={ref as any}
       className={cn(
-        `flex h-full w-full flex-col items-center justify-between overflow-hidden px-2 transition-colors`
+        `flex h-full w-full flex-row items-center justify-between overflow-hidden px-2 transition-colors`
       )}
       style={{
         background: theme?.monitorBgColor || theme?.defaultBgColor || '',
       }}
     >
-      <UniversalPlayer />
-      <PlayerControls />
-      {windowLayout === UIWindowLayout.GRID && (
-        <div className="absolute right-0 top-0 z-20">
-          <div
-            onClick={() => setFullscreen()}
-            className={cn(
-              `cursor-pointer rounded-full p-2`,
-              `transition-all duration-100 ease-in-out`,
-              isFullscreen
-                ? `opacity-0`
-                : `scale-95 opacity-70 hover:scale-100 hover:opacity-100`
-            )}
-          >
-            <MdZoomOutMap className="h-7 w-7" />
+      <div
+        className={cn(
+          `flex h-full w-full flex-col items-center justify-between overflow-hidden`
+        )}
+      >
+        <UniversalPlayer />
+        <PlayerControls />
+        {windowLayout === UIWindowLayout.GRID && (
+          <div className="absolute right-0 top-0 z-20">
+            <div
+              onClick={() => setFullscreen()}
+              className={cn(
+                `cursor-pointer rounded-full p-2`,
+                `transition-all duration-100 ease-in-out`,
+                isFullscreen
+                  ? `opacity-0`
+                  : `scale-95 opacity-70 hover:scale-100 hover:opacity-100`
+              )}
+            >
+              <MdZoomOutMap className="h-7 w-7" />
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
+      <VUMeter className={cn(`ml-1 h-[70%] w-8`)} />
     </div>
   )
 }
